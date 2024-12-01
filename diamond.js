86db57dab94c0ff7bfb508109564dd98e2c060e1
Halim Njeim <michaelnjeim44@gmail.com>
2024-12-01 18:33:10 +0200

    worked out top pattern

--- diamond.js
+++ diamond.js
@@ -1,10 +1,10 @@
 const rows = parseInt(prompt("For Rows: "));
 
-const spaces = rows - 1;
 
 
 for (let i = 1; i < rows + 1; i++) {
-    console.log(spaces * " " + ((i * 2) - 1) * "*")
-
+    let pattern = "* "
+    let space = " "
+    console.log(space.repeat(((rows + 1) - i)) + pattern.repeat(i))
 
 }
