bca47fa8cd410ff96998108660af23095c449340
Halim Njeim <michaelnjeim44@gmail.com>
2024-12-01 18:37:41 +0200

    finished diamond exercise

--- diamond.js
+++ diamond.js
@@ -7,4 +7,9 @@ for (let i = 1; i < rows + 1; i++) {
     let space = " "
     console.log(space.repeat(((rows + 1) - i)) + pattern.repeat(i))
 
+    if (i == rows) {
+        for (let j = rows - 1; j >= 1; j--) {
+            console.log(space.repeat((rows + 1) - j) + pattern.repeat(j))
+        }
+    }
 }
